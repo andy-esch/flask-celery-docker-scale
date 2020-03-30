221eb72449e9302653db39b4c026031957ca3e38
Andy Eschbacher <andyesch@protonmail.com>
2020-03-30 14:39:27 -0400

    minor syntax update

--- flask-app/app.py
+++ flask-app/app.py
@@ -22,7 +22,7 @@ def check_task(id):
     else:
         return str(res.result)
 if __name__ == '__main__':
-    app.run(debug=env.get('DEBUG',True),
-            port=int(env.get('PORT',5000)),
-            host=env.get('HOST','0.0.0.0')
+    app.run(debug=env.get('DEBUG', True),
+            port=int(env.get('PORT', 5000)),
+            host=env.get('HOST', '0.0.0.0')
     )
